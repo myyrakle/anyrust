9d0eb01e752293e6e7aaaedc184664320ba4f3d4
myyrakle <sssang97@naver.com>
2024-05-12 00:19:26 +0900

    [#26] add testcase: test_is_number

--- src/lib.rs
+++ src/lib.rs
@@ -1184,6 +1184,39 @@ mod test_type_check_for_any {
         let a = Any::from(HashMap::new());
         assert!(!a.is_float());
     }
+
+    #[test]
+    fn test_is_number() {
+        let a = Any::new(5.0);
+        assert!(a.is_number());
+
+        let a = Any::new(5.0_f32);
+        assert!(a.is_number());
+
+        let a = Any::new(5.0_f64);
+        assert!(a.is_number());
+
+        let a = Any::new(5);
+        assert!(a.is_number());
+
+        let a = Any::new("5");
+        assert!(!a.is_number());
+
+        let a = Any::new("5.0");
+        assert!(!a.is_number());
+
+        let a = Any::new(true);
+        assert!(!a.is_number());
+
+        let a = Any::new(null);
+        assert!(!a.is_number());
+
+        let a = Any::from(vec![1, 2, 3]);
+        assert!(!a.is_number());
+
+        let a = Any::from(HashMap::new());
+        assert!(!a.is_number());
+    }
 }
 
 lazy_static::lazy_static! {
