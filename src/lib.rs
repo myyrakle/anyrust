472fd8cb832159d59bc5644326c5cf9db0fe3ab6
myyrakle <sssang97@naver.com>
2024-05-12 00:21:00 +0900

    [#26] add testcase: test_is_boolean

--- src/lib.rs
+++ src/lib.rs
@@ -1355,6 +1355,36 @@ mod test_type_check_for_any {
         let a = Any::new(true);
         assert!(!a.is_null());
     }
+
+    #[test]
+    fn test_is_boolean() {
+        let a = Any::new(true);
+        assert!(a.is_boolean());
+
+        let a = Any::new(false);
+        assert!(a.is_boolean());
+
+        let a = Any::new(5.0);
+        assert!(!a.is_boolean());
+
+        let a = Any::new(5);
+        assert!(!a.is_boolean());
+
+        let a = Any::new("5");
+        assert!(!a.is_boolean());
+
+        let a = Any::new("5.0");
+        assert!(!a.is_boolean());
+
+        let a = Any::new(null);
+        assert!(!a.is_boolean());
+
+        let a = Any::from(vec![1, 2, 3]);
+        assert!(!a.is_boolean());
+
+        let a = Any::from(HashMap::new());
+        assert!(!a.is_boolean());
+    }
 }
 
 lazy_static::lazy_static! {
