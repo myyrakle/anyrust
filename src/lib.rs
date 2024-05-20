3d61e0022b2eed56daee0fc52341e86508fdbc24
myyrakle <sssang97@naver.com>
2024-05-21 00:28:50 +0900

    [#47] impl From<char> for Any

--- src/lib.rs
+++ src/lib.rs
@@ -1070,6 +1070,13 @@ impl ToBoolean for f64 {
 impl ToPair for f64 {}
 // ---------------
 
+// 문자 트레잇 구현
+impl From<char> for Any {
+    fn from(value: char) -> Self {
+        Any::new(value.to_string())
+    }
+}
+
 // 문자열 트레잇 구현
 impl From<String> for Any {
     fn from(value: String) -> Self {
@@ -2907,6 +2914,9 @@ impl IntoIterator for Any {
         } else if self.type_id == *MAP {
             let map = self.data.to_map();
             Box::new(map.0.into_iter().map(|(k, v)| Any::from(Pair::new(k, v))))
+        } else if self.type_id == *STRING {
+            let string = self.data.to_string();
+            Box::new(string.chars().map(|c| Any::from(c)))
         } else {
             panic!("Cannot iterate over non-iterable type");
         }
