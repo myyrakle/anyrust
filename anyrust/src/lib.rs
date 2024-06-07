43d34d409cedb1b847b343e032de1f472456f9ea
myyrakle <sssang97@naver.com>
2024-06-08 01:18:29 +0900

    [#59] map! 생성 매크로 구현

--- anyrust/src/lib.rs
+++ anyrust/src/lib.rs
@@ -3294,3 +3294,17 @@ macro_rules! pair {
         }
     };
 }
+
+#[macro_export]
+macro_rules! map {
+    ($($key:expr => $value:expr),*) => {
+        {
+            let mut temp_map = std::collections::HashMap::new();
+            $(
+                temp_map.insert(anyrust::Any::from($key), anyrust::Any::from($value));
+            )*
+
+            anyrust::Any::from(anyrust::Map::from(temp_map))
+        }
+    };
+}
